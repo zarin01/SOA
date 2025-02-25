8a268874a77fa5b28365fee61685342fb8962a00
zarin01 <zarinneugebauer@gmail.com>
2025-02-24 22:52:41 -0700

    Fixing header styles

--- header.php
+++ header.php
@@ -45,10 +45,10 @@
 				</div><!-- .top-navigation-right -->
 			</div><!-- .site-identity-->
 		</div><!-- .container -->
-		<!-- Mobile menu -->
-		<?php get_template_part( 'template-parts/content-menu-drawer' ); ?>
+		
 	</div><!-- .top-navigation -->
-
+<!-- Mobile menu -->
+		<?php get_template_part( 'template-parts/content-menu-drawer' ); ?>
 	<!-- Get the archive page titles -->
 	<?php if ( is_archive() || is_search() || is_404() ) { ?>
 		<div class="container text-container">
